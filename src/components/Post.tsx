6c00324a376a5f0c0bb2ccad740ceeb3a5a91547
Thiago Bittencourt <tblima@proton.me>
2025-09-07 20:19:32 -0300

    add validation on comment form

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -22,7 +22,12 @@ export function Post({ author, publishedAt, content }: { author: any, publishedA
 	}
 
 	function handleNewCommentChange() {
+		event?.target.setCustomValidity('')
 		setNewCommentText(event?.target.value)
+	}
+
+	function handleNewCommentInvalid() {
+		event?.target.setCustomValidity('Esse campo é obrigatório!')
 	}
 
 	function deleteComment(commentToDelete: string) {
@@ -32,11 +37,13 @@ export function Post({ author, publishedAt, content }: { author: any, publishedA
 
 		setComments(commentsWithoutDeletedOne)
 	}
+
+	const isNewCommentEmpty = newCommentText.length == 0
 	
 	return (
-		<article className={styles.post}>
+		<article className={ styles.post }>
 			<header>
-				<div className={styles.author}>
+				<div className={ styles.author }>
 					<Avatar src={ author.avatarUrl } />
 					<div className={styles.authorInfo}>
 						<strong>{ author.name }</strong>
@@ -48,29 +55,29 @@ export function Post({ author, publishedAt, content }: { author: any, publishedA
 				</time>
 			</header>
 
-			<div className={styles.content}>
-				{content.map((item) => {
+			<div className={ styles.content }>
+				{content.map(item => {
 					if(item.type === 'paragraph') {
-						return <p key={item.content}>{item.content}</p>
+						return <p key={ item.content }>{ item.content }</p>
 					} else if(item.type === 'link') {
-						return <p key={item.content}><a href={item.content}>{item.content}</a></p>
+						return <p key={ item.content }><a href={ item.content }>{ item.content }</a></p>
 					}
 				})}
 			</div>
 
-			<form onSubmit={ handleCreateNewComment } className={styles.commentForm}>
+			<form onSubmit={ handleCreateNewComment } className={ styles.commentForm }>
 				<strong>Deixe seu feedback</strong>
 
-				<textarea name="comment" placeholder="Deixe um comentário" value={newCommentText} onChange={ handleNewCommentChange } />
+				<textarea name="comment" placeholder="Deixe um comentário" value={ newCommentText } onChange={ handleNewCommentChange } required onInvalid={ handleNewCommentInvalid } />
 
 				<footer>
-					<button type="submit">Publicar</button>
+					<button type="submit" disabled={ isNewCommentEmpty }>Publicar</button>
 				</footer>
 			</form>
 
 			<div className={styles.commentList}>
 				{ comments.map(comment => {
-					return <Comment key={comment} content={comment} onDeleteComment={deleteComment} />
+					return <Comment key={ comment } content={ comment } onDeleteComment={ deleteComment } />
 				})}
 			</div>
 		</article>
