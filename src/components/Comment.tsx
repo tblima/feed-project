337590ff211cbec753398bdfe4b2c998f3da3283
Thiago Bittencourt <tblima@proton.me>
2025-09-07 20:29:43 -0300

    add like button funcionality

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,10 +1,17 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
+import { useState } from 'react'
 
 export function Comment({content, onDeleteComment}: {content: string, onDeleteComment: (comment: string) => void}) {
+	const [likeCount, setLikeCount] = useState(0)
+	
 	function handleDeleteComment() {
 		onDeleteComment(content)
+	}
+
+	function handleLikeComment() {
+		setLikeCount(likeCount + 1)
 	}
 
 	return (
@@ -27,9 +34,9 @@ export function Comment({content, onDeleteComment}: {content: string, onDeleteCo
 					<p>{ content }</p>
 				</div>
 				<footer>
-					<button>
+					<button onClick={ handleLikeComment }>
 						<ThumbsUp />
-						Aplaudir <span>20</span>
+						Aplaudir <span>{ likeCount }</span>
 					</button>
 				</footer>
 			</div>
